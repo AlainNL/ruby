219fdc383c3f2ed09e1bd632e111e906ad129356
Alain Noel <alain.noel34@gmail.com>
2022-07-29 17:26:43 +0200

    ex 4 ok

---
+++ amusement-park-improvements/attendee.rb
@@ -0,0 +1,36 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  # Do not edit above methods, add your own methods below.
+
+  # rubocop:disable Naming/PredicateName
+  def has_pass?
+    if @pass_id != nil
+      true
+    else
+      false
+    end
+  end
+
+  def fits_ride?(ride_minimum_height)
+    if ride_minimum_height > @height
+      false
+    else
+      true
+    end
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    fits_ride?(ride_minimum_height) && has_pass?
+  end
+end
